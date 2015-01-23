88487820cb3316d6aecb48190dd793da4ceef605
Vigenere36 <Vigenere@lal-98-23.reshall.berkeley.edu>
2015-01-23 14:46:34 -0800

    Better hovering

--- js/main.js
+++ js/main.js
@@ -5,30 +5,29 @@ var initializeMenu = function() {
 	var files = ["index.html", "projects.html", "resume.html", "blog.html", "contact.html"]
 	var ids = ["#homeButton", "#projectsButton", "#resumeButton", "#blogButton", "#contactButton"]
 	var index = files.indexOf(filename)
-	var id = ""
+	var selectedID = ""
 
 	if (index != -1) {
-		id = ids[index]
+		selectedID = ids[index]
 	}
 
-	if (id) {
-		$(id).toggleClass("buttonSelected")
-		$(id).append('<span class="selectedArrow">>></span>')
+	if (selectedID) {
+		$(selectedID).toggleClass("buttonSelected")
+		console.log(selectedID + " > .arrow")
+		$(selectedID + " > .arrow").css({visibility: "visible"})
 	}
 
 	$(".navbar button").hover(
 		function() {
-			if (id.indexOf($(this).attr("id")) == -1) {
+			if (selectedID.indexOf($(this).attr("id")) == -1) {
 				$(this).toggleClass("hovering")
-				$(this).append('<span class="arrow">>></span>')
-				$(".arrow").toggleClass("arrowHover")
+				$("#" + $(this).attr("id") + " > .arrow").toggleClass("arrowHover")
 			}
 		},
 		function() {
-			if (id.indexOf($(this).attr("id")) == -1) {
+			if (selectedID.indexOf($(this).attr("id")) == -1) {
 				$(this).toggleClass("hovering")
-				$(this).find("span:last").remove()
-				$(".arrow").toggleClass("arrowHover")
+				$("#" + $(this).attr("id") + " > .arrow").toggleClass("arrowHover")
 			}
 		}
 
