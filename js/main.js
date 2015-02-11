1663296d09a28df5cfb4c8958ae25c29d9f9395e
Vigenere36 <Vigenere@lal-98-23.reshall.berkeley.edu>
2015-02-10 23:23:52 -0800

    Makeover

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ var initializeMenu = function() {
 	filename = pathname[pathname.length - 1]
 
 	var files = ["index.html", "projects.html", "resume.html", "blog.html", "contact.html"]
-	var ids = ["#homeButton", "#projectsButton", "#resumeButton", "#blogButton", "#contactButton"]
+	var ids = ["#homeLink", "#projectsLink", "#resumeLink", "#blogLink", "#contactLink"]
 	var index = (files.indexOf(filename) == -1) ? 0 : files.indexOf(filename)
-	console.log(index)
 	var selectedID = ""
 
 	if (index != -1) {
@@ -13,30 +12,27 @@ var initializeMenu = function() {
 	}
 
 	if (selectedID) {
-		$(selectedID).toggleClass("buttonSelected")
+		$(selectedID + " a").toggleClass("linkSelected")
 		$(selectedID + " > .arrow").css({visibility: "visible"})
 	}
 
-	$(".navbar button").hover(
+	$(".navbar span").hover(
 		function() {
 			if (selectedID.indexOf($(this).attr("id")) == -1) {
-				$(this).toggleClass("hovering")
 				$("#" + $(this).attr("id") + " > .arrow").toggleClass("arrowHover")
 			}
 		},
 		function() {
 			if (selectedID.indexOf($(this).attr("id")) == -1) {
-				$(this).toggleClass("hovering")
 				$("#" + $(this).attr("id") + " > .arrow").toggleClass("arrowHover")
 			}
 		}
 	)
 
 	//Toggle class off to prevent firefox bug
-	$(".navbar button").click(
+	$(".navbar span").click(
 		function() {
 			if (selectedID.indexOf($(this).attr("id")) == -1) {
-				$(this).toggleClass("hovering", false)
 				$("#" + $(this).attr("id") + " > .arrow").toggleClass("arrowHover", false)
 			}
 	})
